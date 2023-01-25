31a3d959893d8c62ef124272c87753c98080a1c3
SungHyun <62917247+Oein@users.noreply.github.com>
2023-01-25 17:18:03 +0900

    Update plant_the_grass.sh

--- scripts/plant_the_grass.sh
+++ scripts/plant_the_grass.sh
@@ -3,12 +3,6 @@
 current_year=`date +%Y`
 current_month=`date +%b`
 
-monthly_file="$current_month.md"
-
-if [ ! -d $current_year ]; then
-  mkdir $current_year
-fi
-
-cd $current_year
+monthly_file="README.md"
 
 echo -e Today\'s commit date ✨ : `date` \\n >> $monthly_file
